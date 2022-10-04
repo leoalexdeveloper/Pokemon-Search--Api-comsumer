6706ace327acb875542f4146f75766fa429fab0f
Leonardo Pereira <leoalexdeveloper@gmail.com>
2022-10-04 10:41:05 -0300

    fix: elements position on pokemon card

--- src/interfaces/Pokemons.interface.ts
+++ src/interfaces/Pokemons.interface.ts
@@ -1,27 +1,27 @@
 export type PokemonResults = {
-  name: string,
-  url: string
+  name: string;
+  url: string;
 };
 
 export type Pokemons = {
-  count: number,
-  next: string | null,
-  previous: string | null,
-  results: PokemonResults[]
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: PokemonResults[];
 };
 
 export type PokemonInfo = {
-  species: { name: string; url: string },
-  abilities: [{ability: { name: string; url: string }}],
+  species: { name: string; url: string };
+  abilities: [{ ability: { name: string; url: string } }];
   stats: [
     { base_stat: number; effort: number; stat: { name: string; url: string } }
-  ],
-  sprites:{
-    other:{
-      home:{
-        front_default:string
-      }
-    }
-  }
-  weight:string
+  ];
+  sprites: {
+    other: {
+      home: {
+        front_default: string;
+      };
+    };
+  };
+  weight: string;
 };
